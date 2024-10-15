317a24b20f47b64be4a371df1da20e23e7af0369
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-10-16 00:07:56 +0300

    all tasks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Minimum Operations """
+""" Script that computes a minimum operations """
 
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed """
-    n = 'H'
-    b = 'H'
-    o = 0
-    while (len(b) < n):
-        if n % len(b) == 0:
-            o += 2
-            n = b
-            b += b
-        else:
-            o += 1
-            b += n
-    if len(b) != n:
+def minOperations(n):
+    """ Method for compute the minimum number of tasks """
+    if n < 2:
         return 0
-    return o
+    f_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                f_list.append(i)
+    return sum(f_list)
