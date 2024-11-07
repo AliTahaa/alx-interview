142cc0a963f78dbff33d0abe8011a0ca8561c398
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-07 22:00:40 +0200

    all tasks

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,57 @@
 #!/usr/bin/python3
-"""
-Solution to the nqueens problem
-"""
+""" nqueens problem """
 import sys
 
 
-def backtrack(r, n, cols, pos, neg, board):
-    """
-    backtrack function to find solution
-    """
-    if r == n:
-        res = []
-        for g in range(len(board)):
-            for k in range(len(board[g])):
-                if board[g][k] == 1:
-                    res.append([g, k])
-        print(res)
-        return
+def solve(row, size, columns, positive_diagonals, negative_diagonals, chessboard):
+  """ backtrack to find solution """
+  if row == size:
+    result = []
+    for i in range(len(chessboard)):
+      for j in range(len(chessboard[i])):
+        if chessboard[i][j] == 1:
+          result.append([i, j])
+    print(result)
+    return
 
-    for c in range(n):
-        if c in cols or (r + c) in pos or (r - c) in neg:
-            continue
+  for col in range(size):
+    if col in columns or (row + col) in positive_diagonals or (row - col) in negative_diagonals:
+      continue
 
-        cols.add(c)
-        pos.add(r + c)
-        neg.add(r - c)
-        board[r][c] = 1
+    columns.add(col)
+    positive_diagonals.add(row + col)
+    negative_diagonals.add(row - col)
+    chessboard[row][col] = 1
 
-        backtrack(r+1, n, cols, pos, neg, board)
+    solve(row + 1, size, columns, positive_diagonals, negative_diagonals, chessboard)
 
-        cols.remove(c)
-        pos.remove(r + c)
-        neg.remove(r - c)
-        board[r][c] = 0
+    columns.remove(col)
+    positive_diagonals.remove(row + col)
+    negative_diagonals.remove(row - col)
+    chessboard[row][col] = 0
 
 
-def nqueens(n):
-    """
-    Solution to nqueens problem
-    Args:
-        n (int): number of queens. Must be >= 4
-    Return:
-        List of lists representing coordinates of each
-        queen for all possible solutions
-    """
-    cols = set()
-    pos_diag = set()
-    neg_diag = set()
-    board = [[0] * n for i in range(n)]
+def nqueens(size):
+  """ Solution of nqueens """
+  columns = set()
+  positive_diagonals = set()
+  negative_diagonals = set()
+  chessboard = [[0] * size for _ in range(size)]
 
-    backtrack(0, n, cols, pos_diag, neg_diag, board)
+  solve(0, size, columns, positive_diagonals, negative_diagonals, chessboard)
 
 
 if __name__ == "__main__":
-    n = sys.argv
-    if len(n) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        nn = int(n[1])
-        if nn < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-        nqueens(nn)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+  args = sys.argv
+  if len(args) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+  try:
+    n = int(args[1])
+    if n < 4:
+      print("N must be at least 4")
+      sys.exit(1)
+    nqueens(n)
+  except ValueError:
+    print("N must be a number")
+    sys.exit(1)
