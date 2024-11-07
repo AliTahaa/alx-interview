4bf450742499e6053ff0b1dceb08342b2bc8797b
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-11-07 22:05:22 +0200

    all tasks

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,55 +3,58 @@
 import sys
 
 
-def solve(row, size, columns, positive_diagonals, negative_diagonals, chessboard):
-  """ backtrack to find solution """
-  if row == size:
-    result = []
-    for i in range(len(chessboard)):
-      for j in range(len(chessboard[i])):
-        if chessboard[i][j] == 1:
-          result.append([i, j])
-    print(result)
-    return
+def solve(row, size, columns, positive_diagonals,
+          negative_diagonals, chessboard):
+    """ backtrack to find solution """
+    if row == size:
+        result = []
+        for i in range(len(chessboard)):
+            for j in range(len(chessboard[i])):
+                if chessboard[i][j] == 1:
+                    result.append([i, j])
+        print(result)
+        return
 
-  for col in range(size):
-    if col in columns or (row + col) in positive_diagonals or (row - col) in negative_diagonals:
-      continue
+    for col in range(size):
+        if col in columns or (row + col) in positive_diagonals \
+         or (row - col) in negative_diagonals:
+            continue
 
-    columns.add(col)
-    positive_diagonals.add(row + col)
-    negative_diagonals.add(row - col)
-    chessboard[row][col] = 1
+        columns.add(col)
+        positive_diagonals.add(row + col)
+        negative_diagonals.add(row - col)
+        chessboard[row][col] = 1
 
-    solve(row + 1, size, columns, positive_diagonals, negative_diagonals, chessboard)
+        solve(row + 1, size, columns,
+              positive_diagonals, negative_diagonals, chessboard)
 
-    columns.remove(col)
-    positive_diagonals.remove(row + col)
-    negative_diagonals.remove(row - col)
-    chessboard[row][col] = 0
+        columns.remove(col)
+        positive_diagonals.remove(row + col)
+        negative_diagonals.remove(row - col)
+        chessboard[row][col] = 0
 
 
 def nqueens(size):
-  """ Solution of nqueens """
-  columns = set()
-  positive_diagonals = set()
-  negative_diagonals = set()
-  chessboard = [[0] * size for _ in range(size)]
+    """ Solution of nqueens """
+    columns = set()
+    positive_diagonals = set()
+    negative_diagonals = set()
+    chessboard = [[0] * size for _ in range(size)]
 
-  solve(0, size, columns, positive_diagonals, negative_diagonals, chessboard)
+    solve(0, size, columns, positive_diagonals, negative_diagonals, chessboard)
 
 
 if __name__ == "__main__":
-  args = sys.argv
-  if len(args) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-  try:
-    i = int(args[1])
-    if i < 4:
-      print("N must be at least 4")
-      sys.exit(1)
-    nqueens(i)
-  except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        i = int(args[1])
+        if i < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(i)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
